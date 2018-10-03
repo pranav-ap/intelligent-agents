6925ddcb9d1b1d5d5846ced477ab6d24bcf57965
pranav-ap <pranavap96@gmail.com>
2018-10-03 18:47:35 +0530

    small changes

--- simple_vaccum_agent.py
+++ simple_vaccum_agent.py
@@ -1,92 +1,54 @@
-from enum import Enum
-import networkx as nx
+from utils import Action, FloorState, VacuumEnvironment
 import random
 
 
-class FloorState(Enum):
-    CLEAN = 0
-    DIRTY = 1
-
-
-class Action(Enum):
-    NONE = 0
-    MOVE = 1
-    CLEAN = 2
-
-
-class VaccumEnvironment:
-    def __init__(self, height = 3, width = 3):
-        self.width = width
-        self.floor = nx.grid_2d_graph(height, width)
-        # set random floor state values for all the nodes
-        # tile = (location, data)
-        for _, data in self.floor.nodes(data=True):
-            data['status'] = random.choice(list(FloorState))
-
-    def display_floor(self):
-        i = 0
-        print('\n--- Floor ---', end='')
-        for node in self.floor.nodes(data=True):
-            if i % self.width == 0:
-                print ('\n')
-            print(node, end='\t')
-            i += 1
-
-    def get_neighbor_locations(self, location):
-        return [n for n in self.floor.neighbors(location)]
-
-    def get_random_location(self):
-        return random.choice(list(self.floor.nodes()))
-    
-
-class SimpleReflexVaccumAgent:
+class SimpleReflexVacuumAgent:
     def __init__(self, environment):
         self.environment = environment
         self.location = environment.get_random_location()
-    
+
     def get_percept(self):
-        return self.environment.floor.nodes[location]['status']
-    
+        return self.environment.floor.nodes[self.location]['status']
+
     def decide_action(self, percept):
         if percept == FloorState.DIRTY:
             return Action.CLEAN
         return Action.MOVE
 
     def move(self):
-        neighbor_locations = self.environment.get_neighbor_locations(self.location)
+        neighbor_locations = self.environment.get_neighbor_locations(
+            self.location)
         self.location = random.choice(neighbor_locations)
-    
+
     def clean(self):
         self.environment.floor.nodes[self.location]['status'] = FloorState.CLEAN
-        
+
     def perform_action(self, action):
         if action == Action.CLEAN:
             self.clean()
         elif action == Action.MOVE:
             self.move()
 
-    def run(self, limit = 5):
+    def run(self, limit=10):
         count = 0
 
-        while count < limit:
-            self.environment.display_floor()
-            print ('\n--- Current location ---')
-            print(self.location)
+        print('Initial no of dirty tiles : ' +
+              str(self.environment.get_number_of_dirty_tiles()))
 
+        while count < limit:
             percept = self.get_percept()
             action = self.decide_action(percept)
-
-            print('--- Action ---')
-            print(action)
-            
             self.perform_action(action)
 
             count += 1
 
-        
+        print('Final no of dirty tiles : ' +
+              str(self.environment.get_number_of_dirty_tiles()))
+
+
 def main():
-    env = VaccumEnvironment(height = 2, width = 2)
-    agent = SimpleReflexVaccumAgent(environment = env)
+    env = VacuumEnvironment(height=5, width=5)
+    agent = SimpleReflexVacuumAgent(environment=env)
 
     agent.run()
 
---
+++ utils.py
@@ -0,0 +1,53 @@
+from enum import Enum
+import networkx as nx
+import random
+
+
+class FloorState(Enum):
+    CLEAN = 0
+    DIRTY = 1
+
+
+class Action(Enum):
+    NONE = 0
+    MOVE = 1
+    CLEAN = 2
+
+
+class VacuumEnvironment:
+    def __init__(self, height=3, width=3):
+        self.height = height
+        self.width = width
+        self.floor = nx.grid_2d_graph(height, width)
+        # set random floor state values for all the nodes
+        # tile = (location, data)
+        for _, data in self.floor.nodes(data=True):
+            data['status'] = random.choice(list(FloorState))
+
+    def display_floor(self):
+        i = 0
+        print('--- Floor ---', end='')
+        for node in self.floor.nodes(data=True):
+            if i % self.width == 0:
+                print('\n')
+            print(node, end='\t')
+            i += 1
+
+    def get_neighbor_locations(self, location):
+        return list(self.floor.neighbors(location))
+
+    def get_random_location(self):
+        return random.choice(list(self.floor.nodes()))
+
+    def is_all_clean(self):
+        for _, data in self.floor.nodes(data=True):
+            if data['status'] == FloorState.DIRTY:
+                return False
+        return True
+
+    def get_number_of_dirty_tiles(self):
+        count = 0
+        for _, data in self.floor.nodes(data=True):
+            if data['status'] == FloorState.DIRTY:
+                count += 1
+        return count
