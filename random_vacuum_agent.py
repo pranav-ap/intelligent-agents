b81fe578a98e4e1b74c1e6df6252cc9a64cc93c6
Pranav <pranavap96@gmail.com>
2019-03-10 22:58:22 +0530

    random agent update

--- random_vacuum_agent.py
+++ random_vacuum_agent.py
@@ -1,4 +1,4 @@
-from core import RandomAgent2D, Environment2D
+from core import RandomAgent2D, Environment2D, Facing
 from enum import Enum
 
 
@@ -9,10 +9,32 @@ class Action(Enum):
     CLEAN = 3
 
 
+class RandomVacuumAgent2D(RandomAgent2D):
+    def __init__(self, actions, location=(0, 0), facing=Facing.NONE):
+        RandomAgent2D.__init__(self, actions, location, facing)
+
+
+class TrivialEnvironment2D(Environment2D):
+    def initialize_env(self):
+        pass
+
+    def display(self):
+        pass
+
+    def generate_percept(self, agent):
+        pass
+
+    def perform_action(self, agent, action):
+        pass
+
+    def run(self, steps=1000):
+        pass
+
+
 def main():
     actions = [Action.NONE, Action.LEFT, Action.RIGHT, Action.CLEAN]
-    agent = RandomAgent2D(actions=actions)
-    env = Environment2D()
+    agent = RandomVacuumAgent2D(actions=actions)
+    env = TrivialEnvironment2D()
 
 
 if __name__ == '__main__':
